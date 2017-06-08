a51abf6058c273dc28e2ae544f2b784f1a166278
Dominic Torruellas <dtorruellas@gmail.com>
2017-06-07 19:48:12 -0700

    Fixed casing on ApiKey import

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-import apiKEY from './config.json';
+import ApiKey from './config.json';
 
-const API_KEY = apiKEY.API_KEY;
+const API_KEY = ApiKey.API_KEY;
 
 class App extends Component {
   constructor(props) {
