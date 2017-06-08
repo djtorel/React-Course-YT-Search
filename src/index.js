e7c2ea23d6792ade863cd0f5e2b3171f0bd70e7d
Dominic Torruellas <dtorruellas@gmail.com>
2017-06-07 19:29:19 -0700

    First commit, basic app done.

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import apiKEY from './config.json';
+
+const API_KEY = apiKEY.API_KEY;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('synthwave')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      } );
+    });
+  }
+  
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term); }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={ videoSearch } />
+        <div className="row">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList 
+            onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+            videos={ this.state.videos } />
+        </div>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+registerServiceWorker();
